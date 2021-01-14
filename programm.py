24fc2c5dedec518a69552677c8223e6b71c88c6f
Christopher <56773765+DerTopher@users.noreply.github.com>
2021-01-14 13:01:28 +0100

    Create programm.py
    Added empty working file

---
+++ programm.py
@@ -0,0 +1,20 @@
+import random
+
+
+def main():
+    print_header()
+    main_loop()
+
+def main_loop():
+  pass
+
+
+def print_header():
+    print('-------------------------------------')
+    print('           EGGE QUEST')
+    print('-------------------------------------')
+    print()
+
+
+if __name__ == '__main__':
+    main()
